357723430726d419122960c4006dda2321c1022d
lucas francelino de pontes <lucasfrancelinopontes@gmail.com>
2026-05-14 14:52:28 -0300

    bee1184

---
+++ beecrowd/bee1184.py
@@ -0,0 +1,17 @@
+matriz = []
+
+soma = 0
+o = input()
+
+for c in range(12):
+    for l in range(12):
+        v = float(input())
+        if c > l:
+            soma += v
+        matriz.append(v)
+
+
+if o == "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{soma/66:.1f}")
