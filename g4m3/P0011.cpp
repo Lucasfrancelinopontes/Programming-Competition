c2ae7190a108924274537fe371789d98c4de4d05
lucas francelino de pontes <lucasfrancelinopontes@gmail.com>
2026-07-23 17:42:37 -0300

    (g4m3): questão banal

---
+++ g4m3/P0011.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define all(x) (x).begin(), (x).end();
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0);
+
+void solve() {
+    int c; cin >> c;
+    string abu; cin >> abu;
+
+    cout << c-1 << "\n";
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
